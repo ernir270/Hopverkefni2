4f355e2b834161da0ec27b3c37112368421c46fd
Einar <einarlogip@outlook.com>
2021-11-22 01:31:19 +0000

    raða eftir title

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { showTasks } from './lib/tasks.js';
+import { showTaskCount, getCategories, getTags } from './lib/gets.js';
 
 /**
  * Sækir gögn frá data.json
@@ -12,156 +13,11 @@ async function getData(){
 }
 
 // kallar á getData() til að sækja gögn
-const data = await getData();
-const dataItems = data.items;
+export const data = await getData();
+export const dataItems = data.items;
 
-/**
- * Birtir öll tög-in sem eru til.
- */
-function getTags(){
-
-    const tagArr = []; // tómt array sem við munum bæta tags og tag teljara í. (verður tvívítt fylki)
-    let containsIt;
-
-    // bætir öllum tags sem eru til í tagArr
-    for(let i = 0; i < dataItems.length; i++){
-        for(let j = 0; j < dataItems[i].tags.length; j++){
-            containsIt = false; // reset í false
-            
-            // finnur hvort tag er núþegar í listanum 
-            for(let g = 0; g < tagArr.length; g++){
-                // ef tagArr[g] er með dataItems[i].tags[j] í listanum 
-                if(tagArr[g].includes(dataItems[i].tags[j])){
-                    containsIt = true;
-                    tagArr[g][1]++; // bætir 1 við teljarann sem telur hversu mörg task eru með þetta tag
-                    break;
-                }
-            }
-
-            // ef tag er ekki í listanum
-            if(!containsIt){
-                tagArr.push([dataItems[i].tags[j],1]); // bæta tag-i við í listann
-            }
-        }
-    }
-
-    
-    const tagCont = document.querySelector('.tag-container');
-    for(let i = 0; i < tagArr.length; i++){ // búum til element og birtum tag-in
-
-        let liCont = document.createElement('div'); // búa til div
-        liCont.classList.add('li-container') // bæta við class="li-container"
-
-        // búum til <li>
-        let li = document.createElement('li');
-        li.innerText = tagArr[i][0]; // textinn
-        li.classList.add(`${tagArr[i][0]}-tag`);
-        liCont.appendChild(li); // höfum li sem 'barn' af liCont
-
-        // búum til p 
-        let num = document.createElement('p');
-        num.innerText = tagArr[i][1];
-        num.classList.add('grey');
-        num.classList.add('right-num');
-        liCont.appendChild(num);
-
-        tagCont.appendChild(liCont);
-
-        // ef klikkað er á '.${tagArr[i]}-tag'
-        document.querySelector(`.${tagArr[i][0]}-tag`)
-            .addEventListener('click', () => taskSelect(`${tagArr[i][0]}`,'tag'));
-    }
-
-}
-
-/**
- *  Birtir alla flokka sem eru til.
- */
-function getCategories(){
-
-    const category = data.categories;
-    const categoryArray = []; // tvívítt fylki [flokkur][hversu mörg task eru með flokkinn]
-
-    let categoryCount;
-    // bætum við öllum flokkum í categoryArray og líka hversu mörg task eru af þeim flokki
-    for(let i = 0; i < category.length; i++){
-        categoryCount = 0;
-
-        // teljum hversu mörg task eru í flokki category[i].title.toLowerCase()
-        for(let j = 0; j < dataItems.length; j++){
-            if(category[i].title.toLowerCase() === dataItems[j].category){
-                if(!dataItems[j].deleted && !dataItems[j].completed){
-                    categoryCount++;
-                }
-            }
-        }
-
-        categoryArray.push([category[i].title.toLowerCase(), categoryCount]);
-    }
-
-    const categoryContainer = document.querySelector('.category-container');
-    for(let i = 0; i < categoryArray.length; i++){
-
-        let liCont = document.createElement('div'); // búa til div
-        liCont.classList.add('li-container') // bæta við class="li-container"
-
-        let li = document.createElement('li');
-        let categoryTitle = categoryArray[i][0];
-        li.innerText = categoryTitle;
-        li.classList.add(`${categoryTitle}-flokkar`);
-        liCont.appendChild(li); // höfum li sem 'barn' af liCont
-
-        // búum til p 
-        let num = document.createElement('p');
-        num.innerText = categoryArray[i][1];
-        num.classList.add('grey');
-        num.classList.add('right-num');
-        liCont.appendChild(num);
-
-        categoryContainer.appendChild(liCont);
-
-        // ef klikkað er á '.${categoryTitle}-flokkar'
-        document.querySelector(`.${categoryTitle}-flokkar`)
-            .addEventListener('click', () => taskSelect(`${categoryTitle}`,'category'));
-    }
-}
-/**
- * Birtir hversu mörg Verkefni og Kláruð verkefni eru til (sem eru ekki deleted).
- */
-function showTaskCount(){
-
-    let taskCount = 0;
-    let finishedTaskCount = 0;
-
-    for(let i = 0; i < dataItems.length; i++){
-        // ef task er ekki deletað né klárað
-        if(!dataItems[i].deleted && !dataItems[i].completed){
-            taskCount++;
-        }
-        // ef task er klárað og task er ekki deletað
-        if(dataItems[i].completed && !dataItems[i].deleted){
-            finishedTaskCount++;
-        }
-    }
-
-    const parent = document.querySelector('.taskbuttons-container');
-    const liCont = parent.querySelectorAll('.li-container');
-
-
-    // búum til p fyrir Verkefni
-    let num = document.createElement('p');
-    num.innerText = taskCount;
-    num.classList.add('grey');
-    num.classList.add('right-num');
-    liCont[0].appendChild(num);
-
-    // búum til p fyrir Kláruð verkefni
-    num = document.createElement('p');
-    num.innerText = finishedTaskCount;
-    num.classList.add('grey');
-    num.classList.add('right-num');
-    liCont[1].appendChild(num);    
-}
+// breyta sem heldur um hvaða tasks við erum að birta
+export let currentTasks = ['',''];
 
 /**
  * Main fallið.
@@ -184,9 +40,11 @@ async function main(){
  * @param {'category','tag','finished',''} findIn Hvort viltu leita af taskType í category eða tags, eða birta þau task sem eru kláruð.
  *                              Ef '' þá birtir það öll task (nema completed eða deleted task).
  */
-async function taskSelect(taskType, findIn){
+export async function taskSelect(taskType, findIn){
 
     const taskArr = [];
+
+    currentTasks = [taskType, findIn]; // höldum um havað task við erum að sjá.
 
     if(findIn === 'category'){ // leitar í category
         for(let i = 0; i < dataItems.length; i++){
@@ -229,6 +87,8 @@ async function taskSelect(taskType, findIn){
         }
     }
 
+
+    
     showTasks(taskArr);
 }
 
@@ -239,6 +99,13 @@ document.querySelector('.verkefni')
 // ef klikkað er á '.klarud-verkefni'
 document.querySelector('.klarud-verkefni')
     .addEventListener('click', () => taskSelect('','finished'));
+
+
+let dropdown = document.querySelector('#sort-by');
+// þegar það er breytt dropdown value
+// kalla í showTasks() sem mun svo kalla í fallið sem sortar listann.
+dropdown.addEventListener('change', () => taskSelect(currentTasks[0], currentTasks[1]));
+
 
 
 
